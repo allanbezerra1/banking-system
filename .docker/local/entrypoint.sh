beb977778668ba0dd80b3e02779b6281a3643cf0
Allan Bezerra <allanbezerra95@gmail.com>
2024-03-26 11:35:48 -0300

    refactor: update dependencies

--- .docker/local/entrypoint.sh
+++ .docker/local/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+set -e
+
+php artisan storage:link
+php artisan optimize:clear
+php artisan optimize
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# clean database
+php artisan migrate:fresh --seed --force
+
 # Configurar permissões
 chmod 777 -R storage/
 chmod 777 -R bootstrap/cache/
--- .docker/production/entrypoint.sh
+++ .docker/production/entrypoint.sh
@@ -3,12 +3,14 @@
 set -e
 
 php artisan storage:link
-php artisan l5-swagger:generate
+php artisan optimize:clear
 php artisan optimize
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan migrate --seed --force
+
+# clean database
+php artisan migrate:fresh --seed --force
 
 # Configurar permissões
 chmod 777 -R storage/
